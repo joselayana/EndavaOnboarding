87b253dda7a22c73125a355cca3a341958154451
NachoRodVil <ignaciorodvil@gmail.com>
2020-03-19 15:21:41 -0300

    actions/reducers de login y register

--- front/src/redux/reducers/index.js
+++ front/src/redux/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+import loginReducer from "./loginReducer";
+import userReducer from './userReducer';
 
 
 export default combineReducers({
-     
+    login: loginReducer,
+    user: userReducer,
 });
