aafcd587a68281dce59d59e374f76449bf9154fc
NachoRodVil <ignaciorodvil@gmail.com>
2020-03-20 14:25:04 -0300

    rutas recruit

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const recruits = require("./recruit")
 const users = require("./user")
 
 router.use("/api/user", users)
+router.use("/api/recruit", recruits)
 
 
 module.exports = router
