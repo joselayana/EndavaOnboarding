505c8888bcca7153a72d091351423c504921bcc7
joselayana <jooselayan@gmail.com>
2020-03-25 08:55:25 -0300

    links navbar y welcome banner

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -5,16 +5,15 @@ import "../css/style.css"
 export default () => (
 <section id="nav-bar">
   <nav className="navbar navbar-light pt-2">
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="/">
       <img src="images/logo/13.png"/>
-
     </a>
     <div className="dropdown" id= "admin-btn">
     <button className="btn btn-secondary-nav dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       Admin Name
     </button>
        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-         <Link to="/miPerfil" className="dropdown-item" href="#">My Tasks</Link>
+         <Link to="/myTasks" className="dropdown-item" href="#">My Tasks</Link>
          <Link to ="/" className="dropdown-item" href="#">Recruits</Link>
          <Link to ="/" className="dropdown-item" href="#">Dashboard</Link>
          <Link to ="/" className="dropdown-item" href="#">Users</Link>
