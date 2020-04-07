8e705c6feeedd43573ee47d30ffa0c40d6c14d65
CelesteColamarino <celestecolamarino@gmail.com>
2020-04-07 18:23:22 -0300

    Dashboard v2

--- front/src/containers/DashboardContainer.jsx
+++ front/src/containers/DashboardContainer.jsx
@@ -6,24 +6,48 @@ import Dashboard from "../components/Dashboard";
 import Graphics from "../components/Graphics";
 import DashboardRows from "../components/DashboardRows";
 import Progress from "../components/Progress";
+import {searchAllTasks} from "../redux/actions/tasks";
+import {searchRecruits} from "../redux/actions/recruits"
 
 
 class DashboardContainer extends React.Component {
     constructor() {
         super()
+
+    }
+
+    componentDidMount(){
+      this.props.searchAllTasks()
+      this.props.searchRecruits()
     }
 
     render() {
+
+      const {allTasks,allRecruits}=this.props;
+
         return (
             <div>
-                <Dashboard/>
+                <Dashboard allTasks={allTasks}/>
                 <Graphics/>
-                <DashboardRows/>
-                <Progress/>
+                <DashboardRows allTasks={allTasks}/>
+                <Progress allTasks={allTasks} allRecruits={allRecruits}/>
             </div>
         )
     }
 }
 
+const mapStateToProps = (state, ownProps) => ({
+    allTasks: state.task.allTasks,
+    allRecruits:state.recruit.recruits
+})
 
-export default DashboardContainer
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+      searchAllTasks: ()=> dispatch(searchAllTasks()),
+      searchRecruits: ()=> dispatch(searchRecruits())
+    }
+}
+
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(DashboardContainer));
